f8cc3de6f2b3fff224f981a41a2ca26f3a0c3608
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-04-18 17:57:47 +0300

    added-footer

---
+++ frontend/src/pages/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+import Logo from "../../components/Logo";
+
+const Footer = ({ className = "" }) => {
+  return (
+    <footer
+    className={`container mx-auto flex justify-center w-full text-white  gap-4 py-8 bg-slate-900 ${className}`}
+    >
+        <p>©ResQ - 2024</p>
+    
+    </footer>
+  );
+};
+
+export default Footer;
